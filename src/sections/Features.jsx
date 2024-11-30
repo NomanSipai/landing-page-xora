6c11d4d83ee02d83d714e1144c1411e89e50bf9b
Noman Sipai <122717436+NomanSipai@users.noreply.github.com>
2024-11-30 18:44:25 +0530

    add some tailwind css class

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -8,7 +8,10 @@ const Features = () => {
         <div className="container">
           <div className="relative flex md:flex-wrap flex-nowrap border-2 border-s3 rounded-7xl md:overflow-hidden max-md:flex-col feature-after md:g7 max-md:border-none max-md:rounded-none max-md:gap-3">
             {features.map((feature) => (
-              <div key={feature.id} className="">
+              <div
+                key={feature.id}
+                className="relative z-2 md:px-10 px-5 md:pb-10 pb-5 flex-50 max-md:g7 max-md:border-2 max-md:border-s3 max-md:rounded-3xl max-md:320"
+              >
                 {feature.title}
               </div>
             ))}
